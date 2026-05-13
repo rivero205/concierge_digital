4636557b257adbfecb1846b31801096cdbb1ba70
rivero205 <maicolviv695@gmail.com>
2026-05-13 00:57:07 -0500

    feat: integrate GuestProvider, OnboardingFlow into App; remove unused AI files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
+import { GuestProvider, useGuest } from './context/GuestContext'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import CollectionSection from './components/CollectionSection'
 import ConciergeChat from './components/ConciergeChat'
 import ConciergeView from './components/ConciergeView'
+import OnboardingFlow from './components/OnboardingFlow'
 
-export default function App() {
+function AppInner() {
+  const { guest } = useGuest()
   const [showConcierge, setShowConcierge] = useState(
     () => window.location.hash === '#concierge'
   )
@@ -28,24 +31,24 @@ export default function App() {
 
   return (
     <div className="relative bg-black min-h-screen text-cream">
-
-      {/* Fixed full-screen texture overlay */}
       <div
         className="fixed inset-0 z-50 pointer-events-none"
-        style={{
-          backgroundImage: 'url(/texture.png)',
-          backgroundSize: '256px 256px',
-          backgroundRepeat: 'repeat',
-          mixBlendMode: 'lighten',
-          opacity: 0.2,
-        }}
+        style={{ backgroundImage: 'url(/texture.png)', backgroundSize: '256px 256px', backgroundRepeat: 'repeat', mixBlendMode: 'lighten', opacity: 0.2 }}
       />
-
       <HeroSection />
       <AboutSection />
       <CollectionSection onActivate={openConcierge} />
       <ConciergeChat onOpen={openConcierge} hidden={showConcierge} />
-      {showConcierge && <ConciergeView onClose={closeConcierge} />}
+      {showConcierge && !guest.onboardingDone && <OnboardingFlow />}
+      {showConcierge && guest.onboardingDone && <ConciergeView onClose={closeConcierge} />}
     </div>
+  )
+}
+
+export default function App() {
+  return (
+    <GuestProvider>
+      <AppInner />
+    </GuestProvider>
   )
 }
